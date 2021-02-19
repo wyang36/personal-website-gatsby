9fc18a075905a279327a5eb105c5cc77440adedb
wyang36 <kira@knock.com>
2021-02-19 09:40:33 -0800

    try react state form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { navigate } from 'gatsby';
 
 const styles = {
   form: {
@@ -48,6 +49,41 @@ const styles = {
 };
 
 const ContactForm = ({ theme, resume }) => {
+  const [formState, setFormState] = useState({});
+  const formRef = useRef();
+
+  const handleUpdate = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+  const encode = (data) => {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = formRef.current;
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...formState,
+      }),
+    })
+      .then((response) => {
+        console.log('====================================');
+        console.log(`${JSON.stringify(response, null, 2)}`);
+        console.log('====================================');
+        navigate(form.getAttribute('action'));
+      })
+      .catch((error) => {
+        console.log('====================================');
+        console.log(`error in submiting the form data:${error}`);
+        console.log('====================================');
+      });
+  };
   return (
     <div style={{ ...styles.form, color: theme.colorContent2 }}>
       <h1 style={{ textAlign: 'center' }}>Contact Me</h1>
@@ -89,7 +125,15 @@ const ContactForm = ({ theme, resume }) => {
           <FontAwesomeIcon icon={faInstagram} size="lg" />
         </a>
       </div>
-      <form method="post" netlify-honeypot="bot-field" netlify="true" name="contact">
+      <form
+        method="post"
+        netlify-honeypot="bot-field"
+        netlify="true"
+        name="contact"
+        ref={formRef}
+        action="/success"
+        onSubmit={handleSubmit}
+      >
         <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="contact" />
         <label style={{ ...styles.field, color: theme.colorContent2 }}>
@@ -98,6 +142,7 @@ const ContactForm = ({ theme, resume }) => {
             type="text"
             name="name"
             id="name"
+            onChange={handleUpdate}
             style={{
               ...styles.input,
               color: theme.colorContent0,
@@ -111,6 +156,7 @@ const ContactForm = ({ theme, resume }) => {
             type="email"
             name="email"
             id="email"
+            onChange={handleUpdate}
             style={{
               ...styles.input,
               color: theme.colorContent0,
@@ -124,6 +170,7 @@ const ContactForm = ({ theme, resume }) => {
             name="message"
             id="message"
             rows="5"
+            onChange={handleUpdate}
             style={{
               ...styles.input,
               color: theme.colorContent0,
